efd8e34adf756ee45d7444e3d50e1c923a033969
amfriz <amfritz@gmail.com>
2025-05-09 21:14:43 -0400

    added .net api

--- src/app/tasks/no-task/no-task.component.ts
+++ src/app/tasks/no-task/no-task.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-no-task',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './no-task.component.html',
   styleUrl: './no-task.component.css',
 })
-export class NoTaskComponent {}
+export class NoTaskComponent {
+  message = signal('');
+  http = inject(HttpClient);
+
+  onGetMessage() {
+    // this.message.set('No task selected');
+    this.http.get('/api/message', { responseType: 'text' })
+      .subscribe({
+        next: (data) => {
+          this.message.set(data);
+          console.log('Message fetched:', data);
+        },
+        error: (err) => {
+          console.error('Error fetching message:', err);
+          this.message.set('Error fetching message');
+        },
+        complete: () => {
+          console.log('Message fetch complete');
+        } 
+      });
+  }
+}
